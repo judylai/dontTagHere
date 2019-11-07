008bffc5cfae1c7db07b2cd0607e25be639f4b75
Judy Lai <judylai@judys-air.attlocal.net>
2019-11-06 19:55:48 -0800

    changes

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
  const SlackBot = require('slackbots');
- const axios = require('axios');
  const http = require('http');
  const fetch = require('node-fetch');
 
 
  const bot = new SlackBot({
- 	token: 'xoxb-810148513056-797367134898-rCIOc7sPl1uOcjdGi92AlEz4',
+ 	token: 'xoxb-810148513056-817800946311-Jd2X4TGypOQOLrc8WVCCEzMh',
  	name: 'dontTagHere'
  });
 
 
 
 //Start Handler
-bot.on('start', function() {
+bot.on('start', function(event, context) {
 	const params = {
 		icon_emoji: ':dontTagHere1'
 	}; 
@@ -36,29 +35,56 @@ bot.on('start', function() {
  //    	}
 	// });
 
-	const server = http.createServer((req, res) => {
-	 	if (req.method === 'POST') {
-        	res.statusCode = 200;
-		    res.end();
-        	let body = '';
-        	req.on('data', chunk => {
-		        body += chunk.toString(); // convert Buffer to string
-		    });
-		    req.on('end', async () => {
-		        console.log(body);
-		        body = JSON.parse(body);
-		        if(body.event.text.includes('<!here>')) {
-		        	var url = new URL('https://slack.com/api/users.info');
-		        	var params = {user:body.event.user, token: 'xoxb-810148513056-797367134898-rCIOc7sPl1uOcjdGi92AlEz4'};
-		        	url.search = new URLSearchParams(params).toString();
-		        	const response = await fetch(url);
-		        	const myJson = await response.json();
-		        	const name = myJson.user.name;
-		        	bot.postMessageToUser(name, 'Hi there! Instead of tagging @here in the #flex-manager-support channel, please use @flex-mgr-support to get the attention of the Flex Manager team. Thanks!', {icon_emoji: ':dontTagHere1:'});
-		        }
-		    });
-    	}
-	});
+	// const server = http.createServer((req, res) => {
+	//  	if (req.method === 'POST') {
+ //        	let body = '';
+ //        	req.on('data', chunk => {
+	// 	        body += chunk.toString(); // convert Buffer to string
+	// 	    });
+	// 	    req.on('end', async () => {
+	// 	        console.log(body);
+	// 	        body = JSON.parse(body);
+	// 	        if(body.event.text && body.event.text.includes('<!here>')) {
+	// 	        	res.statusCode = 200;
+	// 	    		res.end();
+	// 	        	var url = new URL('https://slack.com/api/users.info');
+	// 	        	var params = {user:body.event.user, token: 'xoxb-810148513056-817800946311-Jd2X4TGypOQOLrc8WVCCEzMh'};
+	// 	        	url.search = new URLSearchParams(params).toString();
+	// 	        	const response = await fetch(url);
+	// 	        	const myJson = await response.json();
+	// 	        	const name = myJson.user.name;
+	// 	        	bot.postMessageToUser(name, 'Hi there! Instead of tagging @here in the #flex-manager-support channel, please use @flex-mgr-support to get the attention of the Flex Manager team. Thanks!');
+	// 	        } else if(body.type === "url_verification") {
+	// 	        	res.statusCode = 200;
+	// 		        res.setHeader('Content-Type', 'text/plain');
+	// 				res.end(body.challenge);
+	// 		    } else {
+	// 		    	res.statusCode = 200;
+	// 	    		res.end();
+	// 		    }
+	// 	    });
+ //    	} 
+	// });
+	async () => {
+		if(context.event.text && context.event.text.includes('<!here>')) {
+	    	res.statusCode = 200;
+			res.end();
+	    	var url = new URL('https://slack.com/api/users.info');
+	    	var params = {user:body.event.user, token: 'xoxb-810148513056-817800946311-Jd2X4TGypOQOLrc8WVCCEzMh'};
+	    	url.search = new URLSearchParams(params).toString();
+	    	const response = await fetch(url);
+	    	const myJson = await response.json();
+	    	const name = myJson.user.name;
+	    	bot.postMessageToUser(name, 'Hi there! Instead of tagging @here in the #flex-manager-support channel, please use @flex-mgr-support to get the attention of the Flex Manager team. Thanks!');
+	    } else if(body.type === "url_verification") {
+	    	res.statusCode = 200;
+	        res.setHeader('Content-Type', 'text/plain');
+			res.end(body.challenge);
+	    } else {
+	    	res.statusCode = 200;
+			res.end();
+	    }
+	};
 
 	server.listen(port, hostname, () => {
 		console.log(`Server running at http://${hostname}:${port}/`);
