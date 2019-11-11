1d1ac1cec674691f1acc2eb343b85de266711b42
Judy Lai <judylai@judys-air.attlocal.net>
2019-11-10 18:05:37 -0800

    Fixes usage of promises
    Fix two instances of Promises not being resovled in Lambda,
    resulting in the second API call to Slack not being made.
    Unresolved Promises in Lambda can result in Lambda prematurely
    terminating the node process.

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
- const SlackBot = require('slackbots');
- const http = require('http');
- const fetch = require('node-fetch');
+const SlackBot = require('slackbots');
+const http = require('http');
+const fetch = require('node-fetch');
+var URL = require('url').;
+var HttpParams = require('url');
 
-
- const bot = new SlackBot({
- 	token: 'xoxb-810148513056-817800946311-Jd2X4TGypOQOLrc8WVCCEzMh',
- 	name: 'dontTagHere'
- });
+const bot = new SlackBot({
+	token: 'xoxb-810148513056-817800946311-Jd2X4TGypOQOLrc8WVCCEzMh',
+	name: 'dontTagHere'
+});
 
 
 exports.bot = async (event, context, callback) => {
 	if(event.event && event.event.text.includes('<!here>')) {
-    	var url = new URL('https://slack.com/api/users.info');
-    	var params = {user:event.event.user, token: 'xoxb-810148513056-817800946311-Jd2X4TGypOQOLrc8WVCCEzMh'};
-    	url.search = new URLSearchParams(params).toString();
-    	const response = await fetch(url);
-    	const myJson = await response.json();
-    	const name = myJson.user.name;
-    	bot.postMessageToUser(name, 'Hi there! Instead of tagging @here in the #flex-manager-support channel, please use @flex-mgr-support to get the attention of the Flex Manager team. Thanks!');
-    } else if(event.type === "url_verification") {
-    	return event.challenge;
-    } else {
-    	return {event};
-    }
-
+		var url = new URL('https://slack.com/api/users.info');
+		var params = {user:event.event.user, token: 'xoxb-810148513056-817800946311-Jd2X4TGypOQOLrc8WVCCEzMh'};
+		url.search = new URLSearchParams(params).toString();
+		const response = await fetch(url);
+		const myJson = await response.json();
+		const name = myJson.user.name;
+		return bot.postMessageToUser(name, 'Hi there! Instead of tagging @here in the #flex-manager-support channel, please use @flex-mgr-support to get the attention of the Flex Manager team. Thanks!');
+	} else if(event.type === "url_verification") {
+		return event.challenge;
+	}
 };
 
 
